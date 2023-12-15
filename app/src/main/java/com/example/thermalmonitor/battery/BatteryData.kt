a273d84f0e011607d758f4c35787b9294cafe075
鹏程 左 <zuopengcheng00@gmail.com>
2023-12-15 20:31:47 +0800

    update

--- app/src/main/java/com/example/thermalmonitor/battery/BatteryData.kt
+++ app/src/main/java/com/example/thermalmonitor/battery/BatteryData.kt
@@ -1,11 +1,12 @@
 package com.example.thermalmonitor.battery
 
-//数据类，用于封装电池信息
+// BatteryData.kt
 data class BatteryData(
-    val level: Int, //电量百分比
-    val status: String, //充电状态
-    val current: Int, //实时电流，单位mA
-    val temperature: Float, //电池温度，单位°C
-    val voltage: Float, //电压，单位V
-    val source: String //供电来源
+    val level: Int, // 电量百分比
+    val status: String, // 充电状态
+    val current: Int, // 实时电流
+    val temperature: Int, // 电池温度
+    val voltage: Int, // 电压
+    val source: String // 供电来源
 )
+
