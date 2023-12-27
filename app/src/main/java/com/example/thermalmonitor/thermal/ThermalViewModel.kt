69b5e0d9c8c755918180330c48bbe03cecacfdc4
鹏程 左 <zuopengcheng00@gmail.com>
2023-12-27 15:14:38 +0800

    ThermalViewModel.kt 优化 fragment_fileslist.xml优化

--- app/src/main/java/com/example/thermalmonitor/thermal/ThermalViewModel.kt
+++ app/src/main/java/com/example/thermalmonitor/thermal/ThermalViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.thermalmonitor.thermal
 
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
@@ -19,18 +23,19 @@ class ThermalViewModel: ViewModel() {
     val thermalList  :LiveData<List<ThermalData>>
         get() = _thermalList
 
+    private var timerJob: Job? = null //用于保存定时任务的协程
+
     init {
-        //在ViewModel的作用域内启动一个线程，在后台线程执行读取和处理数据的任务，每秒执行一次，使用exec()的方式
-        //调用cat命令读取文件内容
-        viewModelScope.launch(Dispatchers.IO){
-            while (true){
+        //在ViewModel的作用域内启动一个协程，在后台线程执行读取和处理数据的任务，每秒执行一次，使用exec函数读取文件内容
+        timerJob = viewModelScope.launch(Dispatchers.IO){
+            while (isActive){ //检查协程是否还在活动状态
                 val list = mutableListOf<ThermalData>()
                 //使用async函数并发读取每个thermal_zone的type以及temp值，返回Deferred对象
                 val deferreds = mutableListOf<Deferred<Pair<String, String>>>()
                 for(i in 0 .. 130){
                     val zone = "thermal_zone$i"
                     deferreds.add(async {
-                        val type = exec("cat /sys/class/thermal/$zone/type").trim()
+                        val type = exec("cat /sys/class/thermal/$zone/type").trim() //使用exec函数执行cat命令读取文件内容
                         val temp = exec("cat /sys/class/thermal/$zone/temp").trim()
                         type to temp //返回一个Pair对象，包含type和tmep 值
 
@@ -43,16 +48,21 @@ class ThermalViewModel: ViewModel() {
                     val zone = "thermal_zone$i"
                     val (type,temp) = results[i]  //解构Pair对象，获取type和temp值
                     if(type.isNotEmpty() && temp.isNotEmpty() && temp != "0" && temp.toInt() > -20000){
-                        list.add(ThermalData(zone,type,String.format("%.2f",temp.toInt()/1000f)))
+                        list.add(ThermalData(zone,type,"%.2f".format(temp.toInt()/1000.0))) //使用字符串模板和双精度浮点数来格式化温度值
                     }
                 }
                 //在主线程更新LiveData的值，通知观察者数据变化
                 withContext(Dispatchers.Main){
-                    _thermalList.postValue(list)
+                    _thermalList.value = list //使用value属性而不是postValue方法来更新LiveData的值
                 }
-                Thread.sleep(1000) //每秒执行一次
+                delay(1000) //使用delay函数而不是Thread.sleep方法来暂停协程
             }
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timerJob?.cancel() //在ViewModel销毁时取消定时任务的协程
     }
 
     //执行命令并返回输出结果的字符串，如果出现异常则返回空字符串
@@ -76,3 +86,4 @@ class ThermalViewModel: ViewModel() {
     }
 
 }
+
