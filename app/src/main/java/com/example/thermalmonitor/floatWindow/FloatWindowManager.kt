7508bda9c3ddd6a4846133b42656cd8f39496201
鹏程 左 <zuopengcheng00@gmail.com>
2023-12-29 15:15:04 +0800

    悬浮窗的 缩放、移动功能搞定，剩余数据显示以及按钮控制

--- app/src/main/java/com/example/thermalmonitor/floatWindow/FloatWindowManager.kt
+++ app/src/main/java/com/example/thermalmonitor/floatWindow/FloatWindowManager.kt
@@ -1,25 +1,31 @@
 package com.example.thermalmonitor.floatWindow
 
 import android.content.Context
+import android.graphics.PixelFormat
 import android.util.Log
 import android.view.WindowManager
 
 class FloatWindowManager(context: Context) {
 
     private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private val params = WindowManager.LayoutParams()
+    private val floatView = FloatWindowView(context).apply {
+        setLayoutParams(params)
+    }
 
-    private val floatView = FloatWindowView(context) // 自定义View
 
     fun show() {
 
         Log.i("show is ok?","yes")
-        val params = WindowManager.LayoutParams()
+
         params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
         params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
         params.width = WindowManager.LayoutParams.WRAP_CONTENT
         params.height = WindowManager.LayoutParams.WRAP_CONTENT
-
+        params.format = PixelFormat.TRANSLUCENT  // 设置为半透明
+        // 设置为半透明
+        floatView.setOpacity(0.8f)
         windowManager.addView(floatView, params)
     }
 
@@ -36,5 +42,8 @@ class FloatWindowManager(context: Context) {
     }
 
     fun isShowing() = floatView.parent != null
+
+
+
 
 }
--- app/src/main/java/com/example/thermalmonitor/floatWindow/FloatWindowView.kt
+++ app/src/main/java/com/example/thermalmonitor/floatWindow/FloatWindowView.kt
@@ -1,41 +1,83 @@
 package com.example.thermalmonitor.floatWindow
 
 import android.content.Context
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.WindowManager
 import android.widget.FrameLayout
+import android.widget.ImageButton
 import com.example.thermalmonitor.R
 
-class FloatWindowView(context: Context) : FrameLayout(context) {
+class FloatWindowView(context: Context, attrs: AttributeSet? = null) : FrameLayout(context, attrs) {
 
-    private var startX = 0
-    private var startY = 0
+    private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private var layoutParams: WindowManager.LayoutParams? = null
+    private var lastX: Float = 0f
+    private var lastY: Float = 0f
+    private var isMinimized: Boolean = false
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.float_window, this)
+        val view = LayoutInflater.from(context).inflate(R.layout.float_window, this)
+        val dragHandle = view.findViewById<ImageButton>(R.id.drag_handle)
+        val minButton = view.findViewById<ImageButton>(R.id.btn_floatWindow_Min)
 
-        // 手指按下记录坐标
-        setOnTouchListener { _, event ->
+        dragHandle.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    startX = event.x.toInt()
-                    startY = event.y.toInt()
+                    lastX = event.rawX
+                    lastY = event.rawY
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val deltaX = event.rawX - lastX
+                    val deltaY = event.rawY - lastY
+                    layoutParams?.let { params ->
+                        params.x += deltaX.toInt()
+                        params.y += deltaY.toInt()
+                        windowManager.updateViewLayout(this, params)
+                    }
+                    lastX = event.rawX
+                    lastY = event.rawY
                 }
             }
-            false
+            true
         }
 
-        // 手指移动,更新悬浮窗位置
-        setOnDragListener { _, event ->
-            val params = (layoutParams as WindowManager.LayoutParams)
-            params.x += (event.x.toInt() - startX)
-            params.y += (event.y.toInt() - startY)
-            layoutParams = params
-            startX = event.x.toInt()
-            startY = event.y.toInt()
-            false
+        minButton.setOnClickListener {
+            isMinimized = !isMinimized
+            updateSize()
         }
     }
 
+    fun setLayoutParams(params: WindowManager.LayoutParams) {
+        this.layoutParams = params
+    }
+
+    private fun updateSize() {
+        layoutParams?.let { params ->
+            params.height = if (isMinimized) dpToPx(50) else dpToPx(400)
+            windowManager.updateViewLayout(this, params)
+        }
+    }
+
+
+
+
+    // dp 转 像素
+    private fun dpToPx(dp: Int): Int {
+        val density = context.resources.displayMetrics.density
+        return (dp * density).toInt()
+    }
+
+
+    // 设置透明度
+    /**
+     * 这个setOpacity方法接受一个0到1之间的浮点数，表示悬浮窗的透明度，0表示完全透明，1表示完全不透明。
+     * */
+    fun setOpacity(opacity: Float) {
+        this.alpha = opacity
+    }
+
 }
+
+
